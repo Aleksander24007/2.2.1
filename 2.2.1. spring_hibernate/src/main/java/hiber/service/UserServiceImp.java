cd3731beb410a513045110ca88bc8203b46f2653
Makedon3 <Makedon3@yandex.ru>
2023-03-11 16:31:10 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -25,5 +25,10 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
+   @Transactional
+   @Override
+   public User getMyCar(String model, Integer series) {
+      return userDao.getMyCar(model,series);
+   }
 
 }
