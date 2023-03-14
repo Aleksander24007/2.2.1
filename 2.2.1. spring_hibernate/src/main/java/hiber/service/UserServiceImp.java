c962559a279f750a48684498f8bfcf8d3a6db590
Makedon3 <Makedon3@yandex.ru>
2023-03-14 15:44:11 +0300

    2.2.1 2-исправление

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -7,25 +7,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 @Service
 public class UserServiceImp implements UserService {
 
    @Autowired
    private UserDao userDao;
 
-   @Transactional
+
    @Override
    public void add(User user) {
       userDao.add(user);
    }
 
-   @Transactional(readOnly = true)
    @Override
    public List<User> getlistUsers() {
       return userDao.getlistUsers();
    }
-   @Transactional
+
    @Override
    public User getMyCar(String model, Integer series) {
       return userDao.getMyCar(model,series);
